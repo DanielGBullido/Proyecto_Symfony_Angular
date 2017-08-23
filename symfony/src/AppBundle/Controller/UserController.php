c3706a1a1d5fff016601d03ffbe0539cdb1531e4
danielgbullido <daniel.gbullido@gmail.com>
2017-08-23 19:00:08 +0200

    add channel function

--- symfony/src/AppBundle/Controller/UserController.php
+++ symfony/src/AppBundle/Controller/UserController.php
@@ -212,4 +212,48 @@ class UserController extends Controller
         }
         return $helpers->json($data);
     }
+
+    public function channelAction(Request $request, $id = null)
+    {
+        $helpers = $this->get("app.helpers");
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getrepository("BackendBundle:User")->findOneBy(
+            array(
+                "id" => $id
+            )
+        );
+
+        $dql = "SELECT v FROM BackendBundle:Video v WHERE v.user = $id ORDER BY v.id DESC";
+        $query = $em->createQuery($dql);
+
+        $page = $request->query->getInt("page", 1);
+        $paginator = $this->get("knp_paginator");
+        $itemsPerPage = 6;
+
+        $pagination = $paginator->paginate($query, $page, $itemsPerPage);
+        $totalItemsCount = $pagination->getTotalItemCount();
+
+        if (count($user) == 1) {
+            $data = array(
+                "status" => "success",
+                "total_items_count" => $totalItemsCount,
+                "page_actual" => $page,
+                "items_per_page" => $itemsPerPage,
+                "total_pages" => ceil($totalItemsCount / $itemsPerPage),
+                "data" => array(
+                    "videos" => $pagination,
+                    "user" => $user
+                )
+            );
+        } else {
+            $data = array(
+                "status" => "error",
+                "code" => 400,
+                "msg" => "user not exist"
+            );
+        }
+        return $helpers->json($data);
+    }
 }
